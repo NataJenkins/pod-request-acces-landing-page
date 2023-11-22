700b100c1963ad34d357774c0fde55fb6de39a67
Natalia Macias <natajenkins@hotmail.com>
2023-11-22 11:39:14 -0600

    mobile fix

--- src/App.jsx
+++ src/App.jsx
@@ -48,52 +48,54 @@ function App() {
 
     return (
         <main>
-            <div className="wrapper">
-                <div className="container">
-                    <img src={Logo} alt="" className="logo" />
-                    <div className="card">
-                        <h1>
-                            Publish your podcasts <br />
-                            <span>everywhere</span>
-                        </h1>
-                        <p>
-                            Upload your audio to Pod with a single click. We’ll
-                            then distribute your podcast to Spotify, Apple
-                            Podcasts, Google Podcasts, Pocket Casts and more!
-                        </p>
-                        <form onSubmit={handleSubmit}>
-                            <div className="input">
-                                <input
-                                    value={formData.email}
-                                    onChange={handleChange}
-                                    type="text"
-                                    id="email"
-                                    name="email"
-                                    placeholder="Email Address"
-                                    className={
-                                        formErrors.email ? "error" : undefined
-                                    }
-                                />
-                                {formErrors.email && (
-                                    <>
-                                        <p className="error-span">
-                                            {formErrors.email}
-                                        </p>
-                                    </>
-                                )}
-                                <button
-                                    type="submit"
-                                    id="submit"
-                                    title="submit"
-                                >
-                                    Request Access
-                                </button>
-                            </div>
-                        </form>
-                        <div className="plataforms">
+            <div className="container">
+                <img src={Logo} alt="" className="logo" />
+                <div className="card">
+                    <h1>
+                        Publish your podcasts <br />
+                        <span>everywhere</span>
+                    </h1>
+                    <p>
+                        Upload your audio to Pod with a single click. We’ll then
+                        distribute your podcast to Spotify, Apple Podcasts,
+                        Google Podcasts, Pocket Casts and more!
+                    </p>
+                    <form onSubmit={handleSubmit}>
+                        <div className="input">
+                            <input
+                                value={formData.email}
+                                onChange={handleChange}
+                                type="text"
+                                id="email"
+                                name="email"
+                                placeholder="Email Address"
+                                className={
+                                    formErrors.email ? "error" : undefined
+                                }
+                            />
+                            {formErrors.email && (
+                                <>
+                                    <p className="error-span">
+                                        {formErrors.email}
+                                    </p>
+                                </>
+                            )}
+                            <button type="submit" id="submit" title="submit">
+                                Request Access
+                            </button>
+                        </div>
+                    </form>
+                    <div className="plataforms">
+                        <div className="image">
                             <img src={Spotify} alt="" />
+                        </div>
+                        <div className="image">
                             <img src={Apple} alt="" />
+                        </div>
+                        <div className="image">
                             <img src={Google} alt="" />
+                        </div>
+                        <div className="image">
                             <img src={Pocket} alt="" />
                         </div>
                     </div>
